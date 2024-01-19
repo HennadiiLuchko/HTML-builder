8b9eb52f190822544f7028c1d8b7001f4486ff14
HennadiiLuchko <137803124+HennadiiLuchko@users.noreply.github.com>
2024-01-19 21:41:10 +0200

    feat; update solution 01-read-file task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -4,4 +4,7 @@ const path = require('path');
 const pathFile = path.join(__dirname, 'text.txt');
 const stream = fs.createReadStream(path.resolve(pathFile));
 
-stream.on('data', (chunk) => console.log(chunk.toString()));
+let data = '';
+
+stream.on('data', (chunk) => console.log(data + chunk.toString()));
+stream.on('error', (error) => console.log('Error', error.message));
